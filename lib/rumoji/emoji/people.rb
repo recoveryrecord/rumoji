8aaed2b60c3f408382f756d55a828397834a0b30
Stuart Argue <stuart@recoveryrecord.com>
2017-03-20 12:56:02 -0700

    Added White Frowning Face

--- lib/rumoji/emoji/people.rb
+++ lib/rumoji/emoji/people.rb
@@ -241,7 +241,8 @@ module Rumoji
       self.new("\u{1F468 200D 2764 FE0F 200D 1F48B 200D 1F468}", [:"man-kiss-man"], "MAN KISS MAN"),
       self.new("\u{1F469 200D 2764 FE0F 200D 1F469}", [:"woman-heart-woman"], "WOMAN HEART WOMAN"),
       self.new("\u{1F469 200D 2764 FE0F 200D 1F48B 200D 1F469}", [:"woman-kiss-woman"], "WOMAN KISS WOMAN"),
-      self.new("\u{1F922}", [:nauseated_face], "NAUSEATED FACE")
+      self.new("\u{1F922}", [:nauseated_face], "NAUSEATED FACE"),
+      self.new("\u{u2639 FE0F}", [:frowning_face], "WHITE FROWNING FACE")
     ]
   end
 end
